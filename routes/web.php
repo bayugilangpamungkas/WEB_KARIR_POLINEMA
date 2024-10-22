19ef6b378c8cc1a8ea92a56cf73a6744feafc2f5
bimocahyo7 <bimochayo@gmail.com>
2024-10-22 17:32:58 +0700

    Edit routing and vite

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
+
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('index');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+// Route untuk dashboard admin
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard'); // Buat view admin.dashboard
+});
+
+require __DIR__ . '/auth.php';
